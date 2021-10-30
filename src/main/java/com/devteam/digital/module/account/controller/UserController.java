efd1aa74f9bbafa3d97f22c226a1404ded0be895
ddthien-coder <jungjihoonkr97@gmail.com>
2021-10-31 01:25:23 +0700

    update

--- src/main/java/com/devteam/digital/module/account/controller/UserController.java
+++ src/main/java/com/devteam/digital/module/account/controller/UserController.java
@@ -7,6 +7,7 @@ import com.devteam.digital.module.account.entity.User;
 import com.devteam.digital.module.account.service.RoleService;
 import com.devteam.digital.module.account.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,16 +24,13 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/v1/digital/users")
 @RequiredArgsConstructor
 public class UserController {
-
-    private final PasswordEncoder passwordEncoder;
+    
     private final UserService userService;
     private final RoleService roleService;
 
     @PostMapping
     @PreAuthorize("@dev.check('user:add')")
     public ResponseEntity<Object> create(@Validated @RequestBody User resources){
-        // Default password 123456
-        resources.setPassword(passwordEncoder.encode("123456"));
         userService.create(resources);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
