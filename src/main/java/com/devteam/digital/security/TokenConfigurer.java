f386d8166e418b4e01de148e35c3060ce3766a16
ddthien-coder <jungjihoonkr97@gmail.com>
2021-10-31 12:57:41 +0700

    update

---
+++ src/main/java/com/devteam/digital/security/TokenConfigurer.java
@@ -0,0 +1,25 @@
+package com.devteam.digital.security;
+
+import com.devteam.digital.security.config.SecurityProperties;
+import com.devteam.digital.security.service.OnlineUserService;
+import com.devteam.digital.security.service.UserCacheClean;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class TokenConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final TokenProvider tokenProvider;
+    private final SecurityProperties properties;
+    private final OnlineUserService onlineUserService;
+    private final UserCacheClean userCacheClean;
+
+    @Override
+    public void configure(HttpSecurity http) {
+        TokenFilter customFilter = new TokenFilter(tokenProvider, properties, onlineUserService, userCacheClean);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
