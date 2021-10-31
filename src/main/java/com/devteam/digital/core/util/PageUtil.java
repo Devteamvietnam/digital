d500b288808ac70e65411e6719956d726c8de90d
ddthien-coder <jungjihoonkr97@gmail.com>
2021-10-31 11:42:13 +0700

    update

---
+++ src/main/java/com/devteam/digital/core/util/PageUtil.java
@@ -0,0 +1,45 @@
+package com.devteam.digital.core.util;
+
+import org.springframework.data.domain.Page;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PageUtil {
+    /**
+     * List
+     */
+    public static List toPage(int page, int size , List list) {
+        int fromIndex = page * size;
+        int toIndex = page * size + size;
+        if(fromIndex > list.size()){
+            return new ArrayList();
+        } else if(toIndex >= list.size()) {
+            return list.subList(fromIndex,list.size());
+        } else {
+            return list.subList(fromIndex,toIndex);
+        }
+    }
+
+    /**
+     * Page Data processing to prevent redis deserialization errors
+     */
+    public static Map<String,Object> toPage(Page page) {
+        Map<String,Object> map = new LinkedHashMap<>(2);
+        map.put("content",page.getContent());
+        map.put("totalElements",page.getTotalElements());
+        return map;
+    }
+
+    /**
+     * Custom pagination
+     */
+    public static Map<String,Object> toPage(Object object, Object totalElements) {
+        Map<String,Object> map = new LinkedHashMap<>(2);
+        map.put("content",object);
+        map.put("totalElements",totalElements);
+        return map;
+    }
+}
--- src/main/java/com/devteam/digital/core/util/SecurityUtils.java
+++ src/main/java/com/devteam/digital/core/util/SecurityUtils.java
@@ -1,13 +1,17 @@
 package com.devteam.digital.core.util;
 
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.devteam.digital.core.util.exception.BadRequestException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.List;
 
 
 /**
@@ -16,13 +20,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 @Slf4j
 public class SecurityUtils {
 
-
+    /**
+     * Get the currently logged in user
+     */
     public static UserDetails getCurrentUser() {
         UserDetailsService userDetailsService = SpringContextHolder.getBean(UserDetailsService.class);
         return userDetailsService.loadUserByUsername(getCurrentUsername());
     }
 
-
+    /**
+     * Get system user name
+     */
     public static String getCurrentUsername() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null) {
@@ -33,6 +41,34 @@ public class SecurityUtils {
             return userDetails.getUsername();
         }
         throw new BadRequestException(HttpStatus.UNAUTHORIZED, "Can't find the current login information");
+    }
+
+    /**
+     * Get system user ID
+     */
+    public static Long getCurrentUserId() {
+        UserDetails userDetails = getCurrentUser();
+        return new JSONObject(new JSONObject(userDetails).get("user")).get("id", Long.class);
+    }
+
+    /**
+     * Get data permissions of the current user
+     */
+    public static List<Long> getCurrentUserDataScope(){
+        UserDetails userDetails = getCurrentUser();
+        JSONArray array = JSONUtil.parseArray(new JSONObject(userDetails).get("dataScopes"));
+        return JSONUtil.toList(array,Long.class);
+    }
+
+    /**
+     * Get data permission level
+     */
+    public static String getDataScopeType() {
+        List<Long> dataScopes = getCurrentUserDataScope();
+        if(dataScopes.size() != 0){
+            return "";
+        }
+        return DataScopeEnum.ALL.getValue();
     }
 
 }
