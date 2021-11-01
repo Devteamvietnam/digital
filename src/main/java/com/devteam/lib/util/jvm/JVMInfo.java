32a4a252b1744e5257d49539f75b572be1129a46
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-01 14:06:51 +0700

    update

--- src/main/java/com/devteam/lib/util/jvm/JVMInfo.java
+++ src/main/java/com/devteam/lib/util/jvm/JVMInfo.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JVMInfo {
-    
+
     private String                          startTime;
     private String                          upTime;
 
