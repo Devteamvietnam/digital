3ba86cdf20226428a59d8a4370ea9331d8bb138e
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-02 00:22:00 +0700

    update

---
+++ src/test/java/com/devteam/util/avatar/AvatarUnitTest.java
@@ -0,0 +1,20 @@
+package com.devteam.util.avatar;
+
+import com.devteam.lib.util.avatar.AvatarUtil;
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class AvatarUnitTest {
+    @Test
+    public void test() throws IOException {
+        String[] names = {"Thien Dinh"};
+        for(String name : names) {
+            BufferedImage img = AvatarUtil.create(150, 150, name);
+            ImageIO.write(img, "png", new File("build/" + name  + ".png"));
+        }
+    }
+}
