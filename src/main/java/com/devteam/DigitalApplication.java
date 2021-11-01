20fe513b29b3d2aa8fd60489e47eacede79e4930
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-01 20:00:49 +0700

    update

--- src/main/java/com/devteam/DigitalApplication.java
+++ src/main/java/com/devteam/DigitalApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DigitalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DigitalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalApplication.class, args);
+    }
 
 }
