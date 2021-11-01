7f5f387483e7b28ea91bc2fecd4219ea403595cd
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-01 13:13:11 +0700

    update

---
+++ src/main/java/com/devteam/lib/util/avatar/AvatarUtil.java
@@ -0,0 +1,5 @@
+package com.devteam.lib.util.avatar;
+
+public class AvatarUtil {
+
+}
---
+++ src/main/java/com/devteam/lib/util/error/ErrorType.java
@@ -0,0 +1,9 @@
+package com.devteam.lib.util.error;
+
+/**
+ * ErrorType
+ */
+public enum ErrorType {
+    IllegalArgument,IllegalState, ConstraintViolation, EntityModified, EntityNotFound,
+    OperationNotAllow, NotAuthorized, LicenseExpired, Unknown
+}
