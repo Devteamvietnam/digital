5f52e5d9ec27354488b96a89155184e783840569
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-02 17:59:09 +0700

    update

---
+++ src/main/java/com/devteam/core/data/db/plugin/ServicePluginService.java
@@ -0,0 +1,27 @@
+package com.devteam.core.data.db.plugin;
+
+import com.devteam.core.data.db.plugin.entity.PluginInfo;
+import com.devteam.core.data.db.plugin.repository.PluginInfoRepository;
+import com.devteam.module.common.ClientInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ServicePluginService {
+    @Autowired
+    private PluginInfoRepository repo;
+
+    public PluginInfo getPlugin(ClientInfo client, String module, String service, String type) {
+        return repo.getOne(module, service, type);
+    }
+
+    public List<PluginInfo> find(ClientInfo client, String module, String service) {
+        return repo.findPluginInfos(module, service);
+    }
+
+    public List<PluginInfo> findAll(ClientInfo client) {
+        return repo.findAll();
+    }
+}
