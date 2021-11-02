45d69872005a9e91d43b8bf6ce060300647a1c14
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-02 16:39:29 +0700

    update

---
+++ src/main/java/com/devteam/core/data/db/AbstractDBModuleScenario.java
@@ -0,0 +1,18 @@
+package com.devteam.core.data.db;
+
+import com.devteam.module.common.ClientInfo;
+import org.springframework.context.ApplicationContext;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+abstract public class AbstractDBModuleScenario {
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void initializeWithTransaction(ClientInfo client, ApplicationContext context, DBScenario scenario) throws Exception {
+        JPAService service = context.getBean(JPAService.class);
+        initialize(client, scenario);
+        service.getEntityManager().flush();
+    }
+
+    abstract public void initialize(ClientInfo client, DBScenario scenario) throws Exception ;
+}
