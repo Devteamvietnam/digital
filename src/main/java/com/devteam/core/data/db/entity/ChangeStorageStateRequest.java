374c5e5b27d50dfabc44de363a0dd703110a71fd
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-02 16:09:10 +0700

    update

---
+++ src/main/java/com/devteam/core/data/db/entity/ChangeStorageStateRequest.java
@@ -0,0 +1,30 @@
+package com.devteam.core.data.db.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.devteam.core.enums.StorageState;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+
+@NoArgsConstructor @Getter @Setter
+public class ChangeStorageStateRequest {
+    @Enumerated(EnumType.STRING)
+    private StorageState newStorageState;
+    private List<Long> entityIds;
+
+    public ChangeStorageStateRequest(StorageState newState) {
+        this.newStorageState = newState;
+    }
+
+    public ChangeStorageStateRequest withEntityId(Long id) {
+        if(entityIds == null) entityIds = new ArrayList<>();
+        entityIds.add(id);
+        return this;
+    }
+}
