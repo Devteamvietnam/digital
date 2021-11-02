1aee3ba0cc7727c29b2f78f409b65ff849e18ce8
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-02 23:00:33 +0700

    fix bug

--- src/main/java/com/devteam/module/DigitalApplication.java
+++ src/main/java/com/devteam/module/DigitalApplication.java
@@ -2,8 +2,17 @@ package com.devteam.module;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableConfigurationProperties
+@SpringBootApplication(
+        exclude = {
+                SecurityAutoConfiguration.class
+        }
+)
 public class DigitalApplication {
 
     public static void main(String[] args) {
