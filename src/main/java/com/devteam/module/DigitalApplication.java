195382d249c1f76f4f5201285c0be4270fe2a46d
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-03 00:08:21 +0700

    update

--- src/main/java/com/devteam/module/DigitalApplication.java
+++ src/main/java/com/devteam/module/DigitalApplication.java
@@ -1,10 +1,16 @@
 package com.devteam.module;
 
+import com.devteam.module.filter.FilterConfig;
+import com.devteam.module.security.WebResourceConfig;
+import com.devteam.module.security.WebSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @EnableConfigurationProperties
@@ -13,10 +19,46 @@ import org.springframework.context.annotation.Configuration;
                 SecurityAutoConfiguration.class
         }
 )
+@Import(value = {
+        WebSecurityConfig.class, WebResourceConfig.class, FilterConfig.class,
+})
 public class DigitalApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DigitalApplication.class, args);
+    static ConfigurableApplicationContext context;
+
+    static public ApplicationContext run(String[] args, long wait) throws Exception {
+        System.out.println(" DEVTEAM SERVER START  \n");
+        context = SpringApplication.run(DigitalApplication.class, args);
+        isRunning(wait);
+        return context;
+    }
+
+    static public boolean isRunning(long waitTime) {
+        boolean running = false;
+        if(waitTime <= 0) waitTime = 1;
+        try {
+            while(!running && waitTime > 0) {
+                if(context == null) running = false;
+                else running = context.isRunning();
+                waitTime -= 100;
+                if(!running && waitTime > 0) Thread.sleep(100);
+            }
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return running;
+    }
+
+    static public void exit() {
+        if(context != null) {
+            SpringApplication.exit(context);
+            context = null;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        run(args, 30000);
+        Thread.currentThread().join();
     }
 
 }
