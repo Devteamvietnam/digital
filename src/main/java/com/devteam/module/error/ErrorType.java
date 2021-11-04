156f6e103327716d468acca192d5efe9721dc377
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-04 19:56:51 +0700

    update

--- src/main/java/com/devteam/module/error/ErrorType.java
+++
@@ -1,6 +0,0 @@
-package com.devteam.module.error;
-
-public enum ErrorType {
-    IllegalArgument,IllegalState, ConstraintViolation, EntityModified, EntityNotFound,
-    OperationNotAllow, NotAuthorized, LicenseExpired, Unknown
-}
