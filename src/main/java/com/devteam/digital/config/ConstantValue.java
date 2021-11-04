193840cf5e34d8e532cabcdaa188f12627f9f782
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-04 19:27:58 +0700

    update

--- src/main/java/com/devteam/digital/config/ConstantValue.java
+++
@@ -1,8 +0,0 @@
-package com.devteam.digital.config;
-
-public class ConstantValue {
-    public static int dummyUserCount = 50;
-    public static int dummyLeaveRequest = 10;
-    public static int dummyMaxLeaveDaysPerSelection = 5;
-    public static int dummyDocuments = 50;
-}
--- src/main/java/com/devteam/digital/dto/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.devteam.digital.dto;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-}
